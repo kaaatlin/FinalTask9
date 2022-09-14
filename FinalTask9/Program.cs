90d272767d91780ab4c0408ed80268c443c3efad
admin <admin@DESKTOP-NKFQO7K>
2022-09-14 20:11:23 +0300

    Добавьте файлы проекта.

---
+++ FinalTask9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/*
+ * Создайте свой тип исключения.
+ * Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. 
+ * Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+ * В блоке catch выведите в консольном сообщении текст исключения.
+ * Создайте консольное приложение, в котором будет происходить сортировка списка фамилий из пяти человек. 
+ * Сортировка должна происходить при помощи события.
+ * Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
+ * Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с использованием собственного типа исключения.
+ */
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception("Собственное исключение");
+            Exception exception1 = new FormatException();
+            Exception exception2 = new IndexOutOfRangeException();
+            Exception exception3 = new TimeoutException();
+            Exception exception4 = new ArgumentException();
+            Exception[] array = { exception, exception1, exception2, exception3, exception4 };
+            foreach (Exception e in array)
+            {
+                try
+                {
+                    throw e;
+                }
+                catch
+                {
+                    Console.WriteLine(e.GetType());
+                    Console.WriteLine("Исключение: " + e.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task2
+{
+    public delegate void SortDelegate(string[] array, int a); // Создание делегата
+    public class Student
+    {
+        public event SortDelegate SortProcess; // Создание события
+        Exception exception = new Exception("Вы не ввели имя студента или имя студента слишком короткое"); // Добавление своего исключения по заданию
+        public void EnterPerson() // Ввод студентов
+        {
+            string[] persons = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine("Введите имя {0} - го человека", i + 1);
+                    persons[i] = Console.ReadLine();
+                    if (persons[i].Length < 2) // Проверка на то, чтобы имя было введено
+                    {
+                        throw exception; // Срабатывает исключение
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Сработало исключение: " + ex.Message);
+                }
+            }
+            ChooseSort(persons); // Вызывает метод, который вызовет событие
+        }
+        protected virtual void ChooseSort(string[] persons)
+        {
+            Console.WriteLine("Выберите способ сортировки: \n1. От А-Я \n2. От Я-А");
+            try
+            {
+                int answer = int.Parse(Console.ReadLine());
+                switch (answer)
+                {
+                    case 1:
+                        {
+                            int choose = answer;
+                            SortProcess?.Invoke(persons, choose); // Вызывает делегат
+                            break;
+
+                        }
+                    case 2:
+                        {
+                            int choose = answer;
+                            SortProcess?.Invoke(persons, choose); // Вызывает делегат
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Выполняется задание 1: использование собственного типа исключений");
+                            throw exception;
+                        }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: {0}\n Попробуйте еще раз", e.Message);
+                ChooseSort(persons);
+            }
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(); 
+            student.SortProcess += Sorting; // Регистрация события
+            student.EnterPerson(); 
+
+        }
+        //Перехватывает событие
+        public static void Sorting(string[] persons, int answer)
+        {
+            string[] newPersons = new string[persons.Length];
+            if (answer == 1)
+            {
+                Array.Sort(persons); // Сортировка от А до Я
+                for (int i = 0; i < persons.Length; i++)
+                {
+                    newPersons[i] = persons[i];
+                    Console.WriteLine(newPersons[i]);
+                }
+            }
+            else
+            {
+                Array.Sort(persons); // Сортировка от Я до А
+                for (int i = persons.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(persons[i]);
+                }
+            }
+
+        }
+    }
+}
